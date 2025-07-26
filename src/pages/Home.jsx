ff5125e886964c8ec19718387a6d61ae374ff25c
Vaibhav <vaibhavranjan420@gmail.com>
2025-07-26 18:01:40 +0530

    Hero Section Done

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import ThemeToggle from '../components/ThemeToggle'
 import StarBackground from '../components/StarBackground'
 import NavBar from '../components/NavBar'
+import HeroSection from '../components/HeroSection'
 
 const Home = () => {
   return (
@@ -8,8 +9,9 @@ const Home = () => {
         <ThemeToggle />
         <StarBackground />
         <NavBar />
+
         <main>
-          
+          <HeroSection />
         </main>
     </div>
   )
