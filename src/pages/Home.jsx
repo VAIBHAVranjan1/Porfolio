ec7ed8f82bc7d2ba55c7080de4b968b35db2682d
Vaibhav <vaibhavranjan420@gmail.com>
2025-07-26 17:10:50 +0530

    Navbar Done

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,17 @@
-import React from 'react'
+import ThemeToggle from '../components/ThemeToggle'
+import StarBackground from '../components/StarBackground'
+import NavBar from '../components/NavBar'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className='min-h-screen bg-background text-foreground overflow-x-hidden'>
+        <ThemeToggle />
+        <StarBackground />
+        <NavBar />
+        <main>
+          
+        </main>
+    </div>
   )
 }
 
